23bb6958dd35d8dc775aa464b37720d4a4755e65
Luis Thomas <thomasluis555@gmail.com>
2017-12-15 17:01:24 -0500

    devise now uses login as an authentication key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
    attr_accessor :login
 

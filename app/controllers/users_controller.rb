95c6140b3b4848b55569d1651bc7876332434c86
Luis Thomas <thomasluis555@gmail.com>
2017-12-29 16:21:45 -0500

    commented out native language whitelist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < Clearance::UsersController
    end
 
    def new
-      
+
       @all_languagues = [ { name: "English", abrv: "en"}, { name: "Spanish", abrv: "es" }, { name: "Portuguese", abrv: 'pt' }, {name: "German", abrv: "de"}, { name: "Greek", abrv: "el"}, { name: "Haitian Creole", abrv: "ht"}, {name: "Hindi", abrv: "hi"}, {name: "Hungarian", abrv: "hu"}, {name: "Indonesian", abrv: "id"}, {name: "Irish", abrv: "ga"}, {name: "Italian", abrv: "it"}, {name: "Japanese", abrv: "ja"}, {name: "Javanese", abrv: "jw"}, {name: "Kannada", abrv: "kn"}, {name: "Korean", abrv: "ko"}, {name: "Kurdish", abrv: "ku"}, {name: "Latin", abrv: "la"}, {name: "Lithuanian", abrv: "lt" }, {name: "French", abrv: "fr" }, {name: "Turkish", abrv: "tr" }, {name: "Bengali", abrv: "bn" }, {name: "Swedish", abrv: "sv" }, {name: "Chinese", abrv: "zh-CN" }, {name: "French", abrv: "fr" } ]
    end
 
@@ -18,14 +18,14 @@ class UsersController < Clearance::UsersController
       email = user_params.delete(:email)
       password = user_params.delete(:password)
       username = user_params.delete(:username)
-      native_language = user_params.delete(:native_language)
+      # native_language = user_params.delete(:native_language)
       # term = user_params.delete(:term)
 
       Clearance.configuration.user_model.new(user_params).tap do |user|
          user.email = email
          user.password = password
          user.username = username
-         user.native_language = native_language
+         # user.native_language = native_language
          # user.term = term
       end
    end
